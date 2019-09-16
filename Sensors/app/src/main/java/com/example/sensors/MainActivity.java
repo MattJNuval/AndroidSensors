00676e817a9d9a65d9e218059de45a0a0b06df0f
Mathew Nuval <nuval.mathewj@gmail.com>
2019-09-16 03:10:55 -0700

    Added some if statements

--- Sensors/app/src/main/java/com/example/sensors/MainActivity.java
+++ Sensors/app/src/main/java/com/example/sensors/MainActivity.java
@@ -20,13 +20,20 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, SensorEventListener {
 
-    TextView textView;
+    private TextView textView;
     private SensorManager sensorManager;
-    Context ctx;
-    TextView content;
-    //Button share;
-    String ver;
-    String output = "";
+    private Context ctx;
+    private TextView content;
+    private String ver;
+    private String output = "";
+    private Sensor sensor;
+    private int type = Sensor.TYPE_ACCELEROMETER;
+    private int pos;
+
+
+    float x =0;
+    float y =0;
+    float z =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +50,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
-
-
-
         ctx     = getApplicationContext();
         content = (TextView) findViewById(R.id.content);
-        //share   = (Button) findViewById(R.id.share);
         ver     = new Build.VERSION().RELEASE;
 
         SensorManager sm          = (SensorManager)ctx.getSystemService(SENSOR_SERVICE);
@@ -98,15 +101,85 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         }
         content.setText(output);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        if(position==1) {
-            content.setText(" ");
-        }
-        else {
+        pos = position;
+
+        if (position == 1) {
+            //Accelerometer
+
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_ACCELEROMETER;
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+
+            content.setText("X: " + x + "\n" +
+                    "Y: " + y + "\n" +
+                    "Z: " + z + "\n");
+
+        } else if (position == 2) {
+            //Linear Accelerometer
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_LINEAR_ACCELERATION;
+            sensor = sensorManager.getDefaultSensor(type);
+
+        } else if (position == 3) {
+            //Gravity Sensor
+            textView.setText("POS: " + pos);
+
+            type = Sensor.TYPE_GRAVITY;
+            sensor = sensorManager.getDefaultSensor(type);
+
+        } else if (position == 4) {
+            //Gyroscope
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_GYROSCOPE;
+            sensor = sensorManager.getDefaultSensor(type);
+
+
+        } else if (position == 5) {
+            //Ambient Light Sensor
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_LIGHT;
+            sensor = sensorManager.getDefaultSensor(type);
+
+
+        } else if (position == 6) {
+            //Ambient Magnetic Field Sensor
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_MAGNETIC_FIELD;
+            sensor = sensorManager.getDefaultSensor(type);
+
+
+        } else if (position == 7) {
+            //Proximity Sensor
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_PROXIMITY;
+            sensor = sensorManager.getDefaultSensor(type);
+
+        } else if (position == 8) {
+            //Pressure Sensor
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_PRESSURE;
+            sensor = sensorManager.getDefaultSensor(type);
+
+        } else if (position == 9) {
+            //Ambient Temperature Sensor
+            textView.setText("POS: " + pos);
+            type = Sensor.TYPE_AMBIENT_TEMPERATURE;
+            sensor = sensorManager.getDefaultSensor(type);
+
+        } else {
+            //All
+            textView.setText("POS: " + pos);
+            sensor = null;
             content.setText(output);
         }
 
@@ -121,10 +194,74 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+       if (pos == 1) {
+            //Accelerometer
+           x = event.values[0];
+           y = event.values[0];
+           z = event.values[0];
+
+
+
+
+        } else if (pos == 2) {
+            //Linear Accelerometer
+
+
+        } else if (pos == 3) {
+            //Gravity Sensor
+
+
+        } else if (pos == 4) {
+            //Gyroscope
+
+
+
+        } else if (pos == 5) {
+            //Ambient Light Sensor
+
+
+        } else if (pos == 6) {
+            //Ambient Magnetic Field Sensor
+
+
+
+        } else if (pos == 7) {
+            //Proximity Sensor
+
+
+        } else if (pos == 8) {
+           //Pressure Sensor
+
+
+        } else if (pos == 9) {
+            //Ambient Temperature Sensor
+
+
+        } else {
+            //All
+            content.setText(output);
+        }
+
+
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
 }
